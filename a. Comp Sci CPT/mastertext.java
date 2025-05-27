dec20bdfbfeeb568409750e897e8bc13ad5b7cd2
kristenli008 <kristen.li26@ycdsbk12.ca>
2025-05-27 11:02:26 -0400

    created master text

--- a. Comp Sci CPT/mastertext.java
+++ a. Comp Sci CPT/mastertext.java
@@ -4,6 +4,19 @@ public class mastertext{
 	public static void main(String[] args){
 		Console con = new Console();
 		
+		TextInputFile animes = new TextInputFile("popular animes.txt");
+		TextInputFile boardgames = new TextInputFile("board games.txt");
+		TextInputFile cartoons = new TextInputFile("cartoons.txt");
+		TextInputFile artprograms = new TextInputFile("digital art programs.txt");
 		
+		int intcount = 0;
+		intcount = cpttools.textcount(cartoons);
+		con.println(intcount);
+		
+		animes.close();
+		boardgames.close();
+		cartoons.close();
+		artprograms.close();
 	}
+	
 }
