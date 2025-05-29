e0ba7b807c808859cefba26af817a58fa5456db8
kristenli008 <kristen.li26@ycdsbk12.ca>
2025-05-29 10:25:42 -0400

    created bubble testing file

---
+++ a. Comp Sci CPT/bubbletest1.java
@@ -0,0 +1,28 @@
+import arc.*;
+
+public class bubbletest1{
+	public static void main(String[] args){
+		Console con = new Console();
+		
+		TextInputFile animes = new TextInputFile("popular animes.txt");
+		String strArray[][];
+		String strRead;
+		int intCols = 0;
+		int intCount;
+				
+		while(animes.eof() == false){
+			strRead = animes.readLine();
+			con.println(strRead);
+			intCols = intCols + 1;
+		}
+		
+		con.println(intCols);
+		
+		strArray = new String[intCols][1];
+		strArray = cpttools.randomsort(animes,intCols);
+		
+		for(intCount = 0; intCount < intCols; intCount++){
+			con.println(strArray[intCount][0] + " - " + strArray[intCount][1]);
+		}
+	}
+}
