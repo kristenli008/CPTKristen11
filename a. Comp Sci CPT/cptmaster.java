2b3e8c96ed456ce0c27aaf3b5c374edbbc9f492d
kristenli008 <kristen.li26@ycdsbk12.ca>
2025-06-03 11:00:34 -0400

    console progress
    figured out colors
    to make buttons: draw rectangles, track x and y position of mouse & button clicks

--- a. Comp Sci CPT/cptmaster.java
+++ a. Comp Sci CPT/cptmaster.java
@@ -3,30 +3,59 @@ import java.awt.Color;
 
 public class cptmaster{
 	public static void main(String[] args){
-		Console con = new Console();
+		Console con = new Console("Hangman",940,700);
 		con.setBackgroundColor(new Color(150,122,158));
+		con.setTextColor(new Color(241,235,250));
+		con.setDrawColor(new Color(241,235,250));
+
+		// color legend
+		// 150,122,158 = middle gray purple, bg
+		// 241,235,250 = extremely light purple, white font color
+		// 97, 66, 143 = darker purple, dark font color
+		
+		// logo
+		// con.println("insert logo");
+		//con.sleep(500);
+		//con.clear();
+		// con.sleep(500);
 		
 		// setting variables
-		String strName;
+		String strName = "name";
 		String strTheme;
 		String strThemename;
 		int intScore;
 		boolean blnerror = false;
 		
 		// setting textinputfiles
-		TextInputFile animes = new TextInputFile("popular animes.txt");
-		TextInputFile boardgames = new TextInputFile("board games.txt");
-		TextInputFile cartoons = new TextInputFile("cartoons.txt");
-		TextInputFile artprograms = new TextInputFile("digital art programs.txt");
+		// TextInputFile animes = new TextInputFile("popular animes.txt");
+		// TextInputFile boardgames = new TextInputFile("board games.txt");
+		// TextInputFile cartoons = new TextInputFile("cartoons.txt");
+		// TextInputFile artprograms = new TextInputFile("digital art programs.txt");
+		TextInputFile mastertext = new TextInputFile("themes.txt");
 		
 		// setting name
-		con.println("Please enter your name:");
-		strName = con.readLine();
+		//con.println("Please enter your name:");
+		//strName = con.readLine();
 		
+		// con.println("Hello, "+strName+"!");
+
 		// getting theme choice
 		while(blnerror == false){
 			con.println("Which theme would you like to play today?");
 			con.println("Please type in the theme name.");
+			
+			// drawing buttons			
+			con.fillRoundRect(50,100,200,50,40,40);
+			con.setDrawColor(new Color(97, 66, 143));
+			con.drawString("Popular Animes",60,100);
+			con.setDrawColor(new Color(241,235,250));
+			
+			con.fillRoundRect(260,100,200,50,40,40);
+			
+			con.fillRoundRect(470,100,200,50,40,40);
+			
+			con.fillRoundRect(680,100,200,50,40,40);
+			
 			con.println("Popular Animes || Board Games || Cartoons || Digital Art Programs");
 			strTheme = con.readLine();
 			String strArray[][];
@@ -35,16 +64,21 @@ public class cptmaster{
 			
 			// running theme gameplay
 			if(strTheme.equalsIgnoreCase("popular animes")){
+				strThemename = mastertext.readLine();
+				TextInputFile animes = new TextInputFile(strThemename+".txt");
 				while(animes.eof() == false){
 					strRead = animes.readLine();
 					intCols = intCols + 1;
 				}
-				strThemename = "popular animes";
-				strArray = cpttools.randomsort(strThemename, intCols);
+				strArray = cpttools.randomsort(strTheme, intCols);
 				intScore = gameplay(strName, strArray, intCols, con);
 				blnerror = true;
 				
+				animes.close();
 			}else if(strTheme.equalsIgnoreCase("board games")){
+				strThemename = mastertext.readLine();
+				strThemename = mastertext.readLine();
+				TextInputFile boardgames = new TextInputFile(strThemename+".txt");
 				while(boardgames.eof() == false){
 					strRead = boardgames.readLine();
 					intCols = intCols + 1;
@@ -54,7 +88,12 @@ public class cptmaster{
 				intScore = gameplay(strName, strArray, intCols, con);
 				blnerror = true;
 				
+				boardgames.close();
 			}else if(strTheme.equalsIgnoreCase("cartoons")){
+				strThemename = mastertext.readLine();
+				strThemename = mastertext.readLine();
+				strThemename = mastertext.readLine();
+				TextInputFile cartoons = new TextInputFile(strThemename+".txt");
 				while(cartoons.eof() == false){
 					strRead = cartoons.readLine();
 					intCols = intCols + 1;
@@ -64,7 +103,13 @@ public class cptmaster{
 				intScore = gameplay(strName, strArray, intCols, con);
 				blnerror = true;
 				
+				cartoons.close();
 			}else if(strTheme.equalsIgnoreCase("digital art programs")){
+				strThemename = mastertext.readLine();
+				strThemename = mastertext.readLine();
+				strThemename = mastertext.readLine();
+				strThemename = mastertext.readLine();
+				TextInputFile artprograms = new TextInputFile(strThemename+".txt");
 				while(artprograms.eof() == false){
 					strRead = artprograms.readLine();
 					intCols = intCols + 1;
@@ -74,6 +119,7 @@ public class cptmaster{
 				intScore = gameplay(strName, strArray, intCols, con);
 				blnerror = true;
 				
+				artprograms.close();
 			}else{
 				con.println("Please enter a valid name.");
 			}
@@ -81,10 +127,6 @@ public class cptmaster{
 		
 		// closing files
 		con.clear();
-		animes.close();
-		boardgames.close();
-		cartoons.close();
-		artprograms.close();
 	}
 	
 	public static int gameplay(String strName, String strArray[][], int intCols, Console con){
